35b20e1bfb844785dcc8239a5e58669257e2f789
Roshan Vyas <roshan.vyas.dev@gmail.com>
2026-08-03 17:08:55 +0530

    chore: setup tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <h1 className="text-4xl font-bold">
+      DevJobs
+    </h1>
+  );
 }
 
-export default App
+export default App;
